4b4185bacf6a27422d0fd8180409643c15eccfcc
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2024-02-20 12:19:56 -0800

    Add files via upload

---
+++ 8ball.py
@@ -0,0 +1,61 @@
+import random
+import time
+
+#This section creates a list of all the possible replies and prints out an ASCII 8ball with a pause between each line. This is to indicate that the code has started running. 
+answers = ["It is certain", "It is decidely so", "Without a doubt", "Yes Definitely", "You may rely on it", "As I see it, yes", "Most Likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot Predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
+print("  .-'''-.")
+time.sleep(.5)
+print(" /   _   \\")
+time.sleep(.5)
+print(" |  (8)  |")
+time.sleep(.5)
+print(" \   ^   /")
+time.sleep(.5)
+print("  '-...-'")
+
+#The main function of the 8ball. 
+def ask_the_ball():
+    #asks the user to input a name and questions. .upper() turns the string into all caps. 
+    name = input("What is your name?")
+    #capitalizes the name
+    cap_name = name.upper()
+    question = input("What is your question?")
+    cap_question = question.upper()
+    #This uses random to create a random integer between two numbers, in this case 0 and the length of the list minus 1 to account for 0 indexing. 
+    get_the_i = random.randint(0, len(answers)-1)
+    #this selectes the answer by using the random number as the index from the list
+    answer = answers[get_the_i]
+
+    #fun filler to pretend like it's thinking
+    print(f"{cap_name}, you asked '{cap_question}'.")
+    print("Pondering the Orb")
+    time.sleep(random.randint(1, 5))
+    print("🔮 ✨ 🔮")
+    time.sleep(1)
+    print(f"{answer}")
+    ask_again = input("Would you like to ask again? yes/no ")
+    #runs play again to check if user wants another round
+    play_again(ask_again)
+
+#if the user answers something other than yes or no, this frunction runs with a new input
+def wrong_answer(user_answer2):
+    play_again(user_answer2)
+
+#play again is the logic for deciding how the player answered about another question
+def play_again(user_answer):
+    #upper case to account for random capitalization
+    more = user_answer.upper()
+    #if yes, runs ask_the_ball again
+    if more == "YES":
+        ask_the_ball()
+    #exits program
+    elif more == "NO":
+        print("Thank you for playing.")
+        exit()
+    #any answer toher than yes or no reminds the user to use an approved answer 
+    else:
+        wrong = input("Please type 'yes' or 'no'. ")
+        wrong_answer(wrong)
+        
+#runs the game function    
+ask_the_ball()
---
+++ README.md
@@ -0,0 +1,7 @@
+# Magic Python 8 ball
+
+A python app that recreates a magic 8 ball. 
+
+to run, type `python3 8ball.py` into your terminal
+
+![gif of the code being run in a terminal](https://thumbs.gfycat.com/WiltedTightBarnacle-mobile.mp4)
